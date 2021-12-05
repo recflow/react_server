82536e1b15c69904e1b22cc4bc84f3872bd24ebf
recflow <chjh9107@gmail.com>
2021-12-06 00:59:59 +0900

    1.2 axios 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
-}
+import React, { Component } from "react";
+import axios from "axios";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hello: [],
+    };
+  }
+  componentDidMount() {
+    this._getDatabase();
+  }
+  _getDatabase = async () => {
+    const res = await axios.get("/api/test");
+    this.setState({ data : res.data });
+    console.log(this.state.data);
+  };
+  render() {
+    return (
+      <>
+        <h3>get DB data(브라우저 개발모드 콘솔확인)</h3>
+      </>
+    );
+  }
+}
 export default App;
